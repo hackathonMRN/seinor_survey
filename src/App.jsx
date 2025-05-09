fcd19d2be3e6db86c462100c023f8da6dad5fd7d
hdongg1 <hdongg.p@gmail.com>
2025-05-10 05:28:20 +0900

    realFinal!

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import QuestionPage from './components/QuestionPage';
 import QuestionSlider from './components/QuestionSlider';
 import ResultPage from './components/ResultPage';
 import './App.css';
@@ -8,8 +9,9 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<QuestionSlider />} />
-        <Route path="/result" element={<ResultPage />} />
+      <Route path="/" element={<QuestionPage />} /> {/* QuestionPage 컴포넌트 사용 */}
+        <Route path="/slider" element={<QuestionSlider />} /> {/* QuestionSlider 컴포넌트 사용 */}
+        <Route path="/result" element={<ResultPage />} /> {/* ResultPage 컴포넌트 사용 */}
       </Routes>
     </Router>
   );
