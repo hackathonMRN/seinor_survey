a83aff01ef35895231c75dda3409e777a5ae8fab
ckg2427 <ckg2427@naver.com>
2025-05-10 03:27:55 +0900

    final

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import QuestionSlider from './components/QuestionSlider';
 import ResultPage from './components/ResultPage';
-import QuestionPage from './components/QuestionPage';
 import './App.css';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<QuestionPage />} /> {/* QuestionPage 컴포넌트 사용 */}
-        <Route path="/slider" element={<QuestionSlider />} /> {/* QuestionSlider 컴포넌트 사용 */}
-        <Route path="/result" element={<ResultPage />} /> {/* ResultPage 컴포넌트 사용 */}
+        <Route path="/" element={<QuestionSlider />} />
+        <Route path="/result" element={<ResultPage />} />
       </Routes>
     </Router>
   );
